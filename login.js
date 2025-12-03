cec1ecd153ce35e4152079dabca74168fd4e4e98
101ping <2557564161@qq.com>
2025-12-03 11:18:54 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,8 @@
+// 获取登录按钮
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click", () => {
+  // 这里未来可以接入真实登录 API
+  // 现在只是演示用途
+  alert("登录逻辑尚未实现，这里是按钮点击事件占位。");
+});
